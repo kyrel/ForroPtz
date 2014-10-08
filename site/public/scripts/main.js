f9f201adf60b39960b39fb8b68b7d5dff65bd6a6
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2014-10-08 20:09:45 +0400

    vk document.ready moved from html to js

--- site/public/scripts/main.js
+++ site/public/scripts/main.js
@@ -1,7 +1,24 @@
 ﻿var app = angular.module('app', []);
 
-app.controller('AuthController', ['$scope', function($scope) {
-    $scope.signIn = function(data) {
+app.controller('AuthController', ['$scope', '$document', function ($scope, $document) {
+
+    $document.ready(function () {
+        if ($("#vk_auth").length) {
+            VK.init({ apiId: 4580391 });
+            VK.Widgets.Auth("vk_auth", {
+                width: "200px",
+                onAuth: function(data) {
+                    //alert('user ' + data['uid'] + ' authorized');
+                    //console.log(data);
+                    $.ajax({ url: '/auth/signIn', type: 'POST', data: data }).done(function() {
+                        window.location.reload();
+                    });
+                }
+            });
+        }
+    });    
+
+    $scope.signIn = function (data) {
     };
 
     $scope.signOut = function() {
